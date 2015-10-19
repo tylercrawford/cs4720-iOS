be8ebf81e738ad522bd7268867aa527bf132690d
Tyler Crawford <tjc4jc@labmini00-4.local>
2015-10-19 16:10:55 -0400

    Initial Commit

---
+++ 100 Things to Do at UVA/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  100 Things to Do at UVA
+//
+//  Created by Tyler Crawford on 10/19/15.
+//  Copyright © 2015 Tyler Crawford. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
