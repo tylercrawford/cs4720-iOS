8b8120f76a7b5f035d7033849ccbd66a02316b75
Tyler Crawford <tjc4jc@labmini02.lab.cs.virginia.edu>
2015-10-19 17:56:12 -0400

    trying to add

--- 100 Things to Do at UVA/ViewController.swift
+++ 100 Things to Do at UVA/ViewController.swift
@@ -8,16 +8,46 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    // MARK: Properties
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var passwordLabel: UILabel!
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        usernameTextField.delegate = self
+        passwordTextField.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: UITextFieldDelegate
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func textFieldDidEndEditing(textField: UITextField) {
+        if (textField == usernameTextField) {
+            usernameLabel.text = textField.text
+
+        }
+        else {
+            passwordLabel.text = "uhhhh"
+        }
+    }
+    
+    // MARK: Actions
+    @IBAction func signInButton(sender: UIButton) {
+        usernameLabel.text = "hello"
     }
 
 
