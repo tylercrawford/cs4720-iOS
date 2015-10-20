801dca20b0397a35149eb9e7fecce4f1f1c0ca22
Tyler Crawford <tjc4jc@labmini00-3.local>
2015-10-19 23:10:32 -0400

    added pinch and dynamic username/password labels

--- 100 Things to Do at UVA/ViewController.swift
+++ 100 Things to Do at UVA/ViewController.swift
@@ -15,14 +15,25 @@ class ViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var usernameLabel: UILabel!
     @IBOutlet weak var passwordLabel: UILabel!
-
+    @IBOutlet weak var registeredUsernameField: UILabel!
+    @IBOutlet weak var registeredPasswordField: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         usernameTextField.delegate = self
         passwordTextField.delegate = self
+        clearRegisterFields()
+        
     }
+    
+    func clearRegisterFields() {
+        registeredUsernameField.text = ""
+        registeredPasswordField.text = ""
+        usernameTextField.text = ""
+        passwordTextField.text = ""
+    }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -36,19 +47,21 @@ class ViewController: UIViewController, UITextFieldDelegate {
     }
     
     func textFieldDidEndEditing(textField: UITextField) {
-        if (textField == usernameTextField) {
-            usernameLabel.text = textField.text
-
-        }
-        else {
-            passwordLabel.text = "uhhhh"
-        }
+        
     }
     
     // MARK: Actions
     @IBAction func signInButton(sender: UIButton) {
-        usernameLabel.text = "hello"
+        let username = usernameTextField.text
+        let password = passwordTextField.text
+        registeredUsernameField.text = "Username: " + username!
+        registeredPasswordField.text = "Password: " + password!
     }
+    
+    @IBAction func handlePinch(recognizer : UIPinchGestureRecognizer) {
+        clearRegisterFields()
+    }
+    
 
 
 }
